b674a3fac74810265873a78ef96fffbdfe2c8a5d
Gabriel Fior <fior.gbm@gmail.com>
2022-11-02 15:56:46 +0100

    Refactored URL

--- src/apy-vision/apy-vision.service.ts
+++ src/apy-vision/apy-vision.service.ts
@@ -8,7 +8,7 @@ export class ApyVisionService {
 
     async getApyPools(): Promise<any[]> {
 
-        const url = 'https://stats.apy.vision/api/v1/pool_search/advanced_search?avg_period_daily_volume_usd=250000&avg_period_reserve_usd=1000000&min_pool_age_days=7&vr=0&exchanges=uniswap_eth&access_token=b55b52c3-3d81-47c5-8d47-91925ce6a6a9';
+        const url = process.env.APY_VISION_URL;
 
         const { data } = await firstValueFrom(
             this.httpService.get<any>(url).pipe(
